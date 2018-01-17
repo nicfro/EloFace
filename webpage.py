ffacfa837682cd380dd689676d37d3f6875af3ea
Nicolai Jacobsen <NJacobsen@dk.imshealth.com>
2018-01-16 21:39:20 -1000

    ???

--- webpage.py
+++ webpage.py
@@ -33,8 +33,9 @@ def postLogin():
 def logout():
     if not session.get('logged_in'):
         return render_template('outside.html')
-    session['logged_in'] = False
-    return redirect('/')
+    else:
+        session['logged_in'] = False
+        return redirect('/')
 
 @app.route('/')
 def home():
@@ -53,12 +54,13 @@ def home():
 def vote():
     if not session.get('logged_in'):
         return render_template('outside.html')
-    choice = request.form['choice']
-    pic1 = request.form['pic1'][46:]
-    pic2 = request.form['pic2'][46:]
-    username = session.get('username')
-    databaseScripts.ratePictures(username, pic1, pic2, choice)
-    return redirect('/')
+    else:
+        choice = request.form['choice']
+        pic1 = request.form['pic1'][46:]
+        pic2 = request.form['pic2'][46:]
+        username = session.get('username')
+        databaseScripts.ratePictures(username, pic1, pic2, choice)
+        return redirect('/')
 
 @app.route('/report', methods = ['POST'])
 def report():
@@ -71,7 +73,8 @@ def report():
 def howto():
     if not session.get('logged_in'):
         return render_template('outside.html')
-    return render_template('howto.html')
+    else:
+        return render_template('howto.html')
 
 @app.route('/newUser/')
 def newUser():
@@ -81,94 +84,97 @@ def newUser():
 def createNewUser():
     if not session.get('logged_in'):
         return render_template('outside.html')
-    username = request.form['username']
-    password = request.form['password']
-    country = request.form['country']
-    email = request.form['email']
-    gender = request.form['gender']
-    bday = request.form['bday']
-    race = request.form['race']
-
-    errors = {}
-
-    try:
-        bday = datetime.strptime(bday, "%Y-%m-%d")
-    except:
-        errors["bdayError"] = "Please enter your birthday"
-
-    if username.isalnum() != True:
-        errors["usernameAlphanumericError"] = "Username can only contain alphanumeric characters"
-    if databaseScripts.userExists(username):
-        errors["usernameExistsError"] = "Username is taken, please choose another"
-    if len(password) < 6:
-        errors["passwordLengthError"] = "Password must be 6 characters or longer"
-    if not validate_email(email):
-        errors["validEmailError"] = "Please enter valid email"
-    if gender == "Please select your gender":
-        errors["genderError"] = "Please select your gender"
-    if country == "Please select your country":
-        errors["countryError"] = "Please select your country"
-    if race == "Please select your ethnicity":
-        errors["ethnicityError"] = "Please select your ethnicity"
-
-    if len(errors) == 0:
-        databaseScripts.CreateNewUser(username, password, country, email, gender, bday, race)
-        session['logged_in'] = True
-        session['username'] = username
-        return Response(home(),mimetype = "text/html")
     else:
-        json_data = json.dumps(errors)
-        return Response(json_data ,mimetype = "application/json")
+        username = request.form['username']
+        password = request.form['password']
+        country = request.form['country']
+        email = request.form['email']
+        gender = request.form['gender']
+        bday = request.form['bday']
+        race = request.form['race']
+
+        errors = {}
+
+        try:
+            bday = datetime.strptime(bday, "%Y-%m-%d")
+        except:
+            errors["bdayError"] = "Please enter your birthday"
+
+        if username.isalnum() != True:
+            errors["usernameAlphanumericError"] = "Username can only contain alphanumeric characters"
+        if databaseScripts.userExists(username):
+            errors["usernameExistsError"] = "Username is taken, please choose another"
+        if len(password) < 6:
+            errors["passwordLengthError"] = "Password must be 6 characters or longer"
+        if not validate_email(email):
+            errors["validEmailError"] = "Please enter valid email"
+        if gender == "Please select your gender":
+            errors["genderError"] = "Please select your gender"
+        if country == "Please select your country":
+            errors["countryError"] = "Please select your country"
+        if race == "Please select your ethnicity":
+            errors["ethnicityError"] = "Please select your ethnicity"
+
+        if len(errors) == 0:
+            databaseScripts.CreateNewUser(username, password, country, email, gender, bday, race)
+            session['logged_in'] = True
+            session['username'] = username
+            return Response(home(),mimetype = "text/html")
+        else:
+            json_data = json.dumps(errors)
+            return Response(json_data ,mimetype = "application/json")
 
 @app.route('/upload/')
 def upload():
     if not session.get('logged_in'):
         return render_template('outside.html')
-    return render_template('upload.html')
+    else:
+        return render_template('upload.html')
 
 @app.route('/uploadToS3', methods = ['POST'])
 def uploadToS3():
     if not session.get('logged_in'):
         return render_template('outside.html')
-
-    errors = {}
-
-    username = session.get('username')
-    try:
-        image = request.form['upload']
-    except:
-        errors["imageError"] = "Please upload an image"
-
-    gender = request.form['gender']
-    race = request.form['race']
-    ageGroup = request.form['ageGroup']
-
-    if gender == "Please select image gender":
-        errors["genderError"] = "Please select image gender"
-    if race == "Please select image ethnicity":
-        errors["ethnicityError"] = "Please select image ethnicity"
-    if ageGroup == "Please select image age group":
-        errors["ageError"] = "Please select image age group"
-    if len(errors) == 0:
-        fileName = databaseScripts.getNewFileName()
-        image_data = re.sub('^data:image/.+;base64,', '', image)
-        encoded = BytesIO(base64.b64decode(image_data.encode()))
-
-        databaseScripts.uploadS3Image(encoded, fileName)
-        databaseScripts.uploadImage(fileName, username, gender, race, ageGroup)
-        resp = json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-        return resp
     else:
-        json_data = json.dumps(errors)
-        return Response(json_data ,mimetype = "application/json")
+        errors = {}
+
+        username = session.get('username')
+        try:
+            image = request.form['upload']
+        except:
+            errors["imageError"] = "Please upload an image"
+
+        gender = request.form['gender']
+        race = request.form['race']
+        ageGroup = request.form['ageGroup']
+
+        if gender == "Please select image gender":
+            errors["genderError"] = "Please select image gender"
+        if race == "Please select image ethnicity":
+            errors["ethnicityError"] = "Please select image ethnicity"
+        if ageGroup == "Please select image age group":
+            errors["ageError"] = "Please select image age group"
+        if len(errors) == 0:
+            fileName = databaseScripts.getNewFileName()
+            image_data = re.sub('^data:image/.+;base64,', '', image)
+            encoded = BytesIO(base64.b64decode(image_data.encode()))
+
+            databaseScripts.uploadS3Image(encoded, fileName)
+            databaseScripts.uploadImage(fileName, username, gender, race, ageGroup)
+            resp = json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+            return resp
+        else:
+            json_data = json.dumps(errors)
+            return Response(json_data ,mimetype = "application/json")
 
 @app.route('/highscores/')
 def highscores():
     if not session.get('logged_in'):
         return render_template('outside.html')
-    highscores = databaseScripts.getHighscores("female")
-    highscores = [["https://s3-eu-west-1.amazonaws.com/ratemegirl/"+x[0],x[1], i] for i, x in enumerate(highscores, 1)]
-    return render_template('highscores.html', highscores = highscores)
+    else:
+        highscores = databaseScripts.getHighscores("female")
+        highscores = [["https://s3-eu-west-1.amazonaws.com/ratemegirl/"+x[0],x[1], i] for i, x in enumerate(highscores, 1)]
+        return render_template('highscores.html', highscores = highscores)
 
 @app.route('/adminReport/')
 def adminReport():
@@ -178,11 +184,12 @@ def adminReport():
 def adminUsers():
     if not session.get('logged_in'):
         return render_template('outside.html')
-    userImages = databaseScripts.getUserInfo("images")
-    userVotes = databaseScripts.getUserInfo("vote")
-    userReports = databaseScripts.getUserInfo("report")
-    return render_template('adminUsers.html', userImages = userImages,
-                            userVotes = userVotes, userReports = userReports)
+    else:
+        userImages = databaseScripts.getUserInfo("images")
+        userVotes = databaseScripts.getUserInfo("vote")
+        userReports = databaseScripts.getUserInfo("report")
+        return render_template('adminUsers.html', userImages = userImages,
+                                userVotes = userVotes, userReports = userReports)
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
